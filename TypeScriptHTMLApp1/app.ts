176fd1e9966e662fabb28fd4368a8a864ead49c4
Nick Chung <nchung@medimpactdirect.com>
2016-02-08 16:57:20 -0800

    Initial Commit

---
+++ TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,58 @@
+﻿enum DiscFlags {
+    None = 0,
+    Drive = 1,
+    Influence = 2,
+    Steadiness = 4,
+    Conscientiousness = 8
+};
+
+class ClickCounter {
+    private count = 0;
+    //registerClick() {
+    registerClick = () => {
+        this.count++;
+        alert(this.count);
+    }
+}
+
+class Greeter {
+
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.span.innerText = new Date().toUTCString();
+
+    }
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+
+    stop() {
+        clearTimeout(this.timerToken);
+    }
+
+}
+
+window.onload = () => {
+    var el = document.getElementById('content');
+
+    //var greeter = new Greeter(el);
+    //greeter.start();
+
+    var clickCounter = new ClickCounter();
+    //document.getElementById('target').onclick = clickCounter.registerClick;
+    //clickCounter.registerClick();
+    //clickCounter.registerClick();
+
+    document.getElementById('target').onclick = clickCounter.registerClick;
+
+     
+};
